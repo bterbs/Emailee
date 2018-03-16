2cbb3bc28dc7644c76f7a05c70416e29e6a55bf1
B Terbush <brittanyterbush@gmail.com>
2018-03-16 10:48:58 -0700

    install passport and google strategy

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Yo Miles tho :) I still work?' });
-});
+passport.use(new googleStrategy());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
