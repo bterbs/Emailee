2d3d26cb09e6d52e3240a0d845680e4202ebc71d
B Terbush <brittanyterbush@gmail.com>
2018-03-24 16:35:10 -0700

    Add schema for mongoose collection

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const keys = require('./config/keys');
 const mongoose = require('mongoose');
 require('./services/passport');
+require('./models/user');
 
 mongoose.connect(keys.mongoURI);
 const app = express();
@@ -10,3 +11,4 @@ require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+console.log('Server is running on port 5000');
