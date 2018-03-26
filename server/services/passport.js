db3d8e824ba0341e25bc607d1a7e9663ea0afb43
B Terbush <brittanyterbush@gmail.com>
2018-03-26 13:43:47 -0700

    Insert logic in passport.use method to check if user in db, else add to db

--- server/services/passport.js
+++ server/services/passport.js
@@ -13,6 +13,14 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleID: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // we have a record with the given profile id
+        } else {
+          // we do not have a record with the id; create a new record in db
+        }
+      });
+
       new User({ googleID: profile.id }).save();
     }
   )
