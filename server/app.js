0cb523a8fe733ec2c4f900530f6ba99645ea0b5f
B Terbush <brittanyterbush@gmail.com>
2018-04-10 18:04:51 -0700

    reformat if statement

--- server/app.js
+++ server/app.js
@@ -33,6 +33,6 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+const port = process.env.PORT || 5000;
+app.listen(port);
 console.log('Server is running on port 5000');
